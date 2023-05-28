129291ee1799b541a8438f1f62bc9ef055e3ee4a
0-cool <jfco.rojas@hotmail.com>
2023-05-28 12:17:33 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
+
+async function CorotosGetData(search){
+    const url = 'https://www.corotos.com.do/k/' + search + '?q%5Bsorts%5D=price_dop%20asc'; // + search
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto(url);
+    const bookData = await page.evaluate(() => {
+
+        const convertPrice = (price) => { return parseFloat(price); }
+        const bookPods = Array.from(document.querySelectorAll('.page_content .flex.group'));
+        const data = bookPods.map((book) => ({
+            title: book.querySelector('.listing-bottom-info h3').innerText,
+            img: book.querySelector('a img').getAttribute('src'),
+            currency: book.querySelector('.listing-bottom-info .price-info span.text-overline').innerText,
+            price: convertPrice(book.querySelector('.listing-bottom-info .price-info span.text-title-3').innerText),
+            company: 'Corotos'
+        }));
+        return data;
+    }, url)
+ 
+    browser.close();
+    return bookData;
+}
+
+app.get('/api//corotos/:search', async (req, res) => {
+    const {search} = req.params;
+    const dataManage = await CorotosGetData(search);
+    res.status(200).send(dataManage);
+})
+
+/*
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It works!'
+    });
+});
+*/
+
+module.exports = app;
